646d495ad11bf999f2adc88a48367c3cbbfafd96
AreloXD2 <bybuitronva@uide.edu.ec>
2026-03-20 18:06:27 +0000

    key exportion

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -2,3 +2,4 @@ uname -a # daba información del ordenador
 which gpg # ubicación del binario de gpg
 gpg --version # me da la versión e info adicional
 gpg --full-generate-key # Genero llave publica y privada 
+gpg --armor --export # Importar la llave publica a un archivo
