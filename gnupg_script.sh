8bb24b47ddc4679fa463bab9200a689304802144
AreloXD2 <bybuitronva@uide.edu.ec>
2026-03-26 12:43:48 +0000

    export private key

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -5,3 +5,4 @@ gpg --full-generate-key # Genero llave publica y privada
 gpg --armor --export # Importar la llave publica a un archivo
 gpg --list-keys # Listar las llaves
 gpg --list-secret-keys --keyid-format=long # listar llave privada
+gpg --armor --export-secret-keys XXXXXXXX # Exportamos la llave privada
