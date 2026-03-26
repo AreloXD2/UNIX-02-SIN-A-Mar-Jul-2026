ddb6427599bed4137f3e3b0343c5e6354ccb0032
AreloXD2 <bybuitronva@uide.edu.ec>
2026-03-26 12:58:15 +0000

    export public key

--- gnupg_script.sh
+++ gnupg_script.sh
@@ -6,3 +6,4 @@ gpg --armor --export # Importar la llave publica a un archivo
 gpg --list-keys # Listar las llaves
 gpg --list-secret-keys --keyid-format=long # listar llave privada
 gpg --armor --export-secret-keys XXXXXXXX # Exportamos la llave privada
+gpg --armor --export buitronariel23@gmail.com > mi_llave_publica.asc # exportar mi llave publica
