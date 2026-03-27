fb26154aabdd16965ba7c721eaaf82f1f07a53f9
AreloXD2 <bybuitronva@uide.edu.ec>
2026-03-27 16:41:31 +0000

    wip: process as files

--- filesystem_script.sh
+++ filesystem_script.sh
@@ -2,3 +2,7 @@
 cd /dev
 # List files and directories on the /dev directory
 ls 
+# Navigate to the /proc directory for system info
+cd /proc
+# Display CPU architecture and processor details
+cat cpuinfo 
