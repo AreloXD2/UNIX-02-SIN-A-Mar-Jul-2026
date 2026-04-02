d07d2bc45eec5f8740849d7ba2fd8a9ff388ed5a
AreloXD2 <bybuitronva@uide.edu.ec>
2026-04-02 12:51:54 +0000

    wip: Update system packages and install Parted for disk management

--- fs_exploration.sh
+++ fs_exploration.sh
@@ -2,3 +2,9 @@
 cd /
 #Lists the contents of a directory, adding symbols to indicate file types.
 ls -F 
+#Updates the package list from repositories
+sudo apt update
+#Upgrades all installed packages to the latest versions
+sudo apt upgrade
+#Installs the Parted tool for disk partition management
+sudo apt install parted
