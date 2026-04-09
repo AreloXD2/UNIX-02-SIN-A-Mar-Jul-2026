f576444732ffbda55650a1badae49e2400f3dea1
AreloXD2 <bybuitronva@uide.edu.ec>
2026-04-09 12:26:24 +0000

    wip: relative and absolute path

---
+++ script_boot.sh
@@ -0,0 +1,10 @@
+# A new directory named “project” is created in the current location
+mkdir proyecto
+#Lists the files and folders in the current directory (it should display “project”)
+ls
+#We move to the “project” directory using a relative path
+cd proyecto/
+#We go back to the previous directory (leaving “project”), also using a relative path
+cd ..
+#We access the “project” folder directly using a full absolute path
+cd /workspaces/UNIX-02-SIN-A-Mar-Jul-2026/proyecto
