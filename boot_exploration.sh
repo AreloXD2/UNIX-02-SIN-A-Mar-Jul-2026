d807b2027a03c1e5b448dbf5b7edb90ac355edd4
AreloXD2 <bybuitronva@uide.edu.ec>
2026-04-16 12:55:31 +0000

    wip: Add examples of mkdir and rmdir using -- to handle directory names starting with hyphens

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -2,3 +2,7 @@
 ls -l -a -h
 #Same as above using the combined flag version
 ls -lah
+#Create a directory named "-rf" (the -- prevents it from being treated as an option)
+mkdir -- -rf
+#Remove the directory named "-rf" using --
+rmdir -- -rf
