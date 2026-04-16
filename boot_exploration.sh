98e18f7d04f7b1a64510753d4791cddc696d058c
AreloXD2 <bybuitronva@uide.edu.ec>
2026-04-16 13:12:13 +0000

    wip: Document how to use man git-clone and navigate with /, n, and N

--- boot_exploration.sh
+++ boot_exploration.sh
@@ -6,3 +6,13 @@ ls -lah
 mkdir -- -rf
 #Remove the directory named "-rf" using --
 rmdir -- -rf
+#Displays the manual page for the git-clone command
+man git-clone
+# I used "man git-clone" to look up detailed documentation about the command. Inside the manual, I navigated using:
+#   - / to search for keywords in this case depth
+#   - n to move to the next search result
+#   - N to move to the previous search result
+# This helped me quickly find relevant sections and understand how git clone works
+--depth <depth>
+           Create a shallow clone with a history truncated to the specified number of commits. Implies --single-branch unless --no-single-branch is given to fetch the
+           histories near the tips of all branches. If you want to clone submodules shallowly, also pass --shallow-submodules.
