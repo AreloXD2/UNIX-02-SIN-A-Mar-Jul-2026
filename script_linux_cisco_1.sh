f814144e06e9edbf5e20583d209e1dbf2d7d9047
AreloXD2 <bybuitronva@uide.edu.ec>
2026-05-03 21:15:04 +0000

    wip: ls command with directory argument

--- script_linux_cisco_1.sh
+++ script_linux_cisco_1.sh
@@ -1,3 +1,5 @@
 Linux course commands
 #Lists the files and directories in the current working directory
 ls
+#Lists the contents of a specific directory (in this case, "Documents")and acts as an argument for the command
+ls Documents
