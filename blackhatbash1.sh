0d9d7f7062c930773f61ec214dced6ee01f27d98
AreloXD2 <bybuitronva@uide.edu.ec>
2026-06-04 13:01:44 +0000

    wip: Blackhatbash commands debbuging

--- blackhatbash1.sh
+++ blackhatbash1.sh
@@ -1,6 +1,19 @@
+#!/usr/bin/env bash
+set -x
 bash --version
+set -x
 env 
+set +x
 echo ${SHELL}
 echo ${RANDOM}
 echo ${UID}
 echo ${OSTYPE}
+
+ps -e -f
+ps -ef
+df --human-readable
+# bash -r blackhatbash1.sh
+# bash -n blackhatbash1.sh
+# bash -x script.sh
+# man
+set +x
