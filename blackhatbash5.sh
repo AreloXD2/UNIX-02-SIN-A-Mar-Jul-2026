f40df76fc03248e0714e0ad5622ff9277337ff31
AreloXD2 <bybuitronva@uide.edu.ec>
2026-06-05 17:17:32 +0000

    wip: Examples of I/O redirection

--- blackhatbash5.sh
+++ blackhatbash5.sh
@@ -5,3 +5,20 @@ top
 top&
 # Kills the command using ID command
 Kill -9 
+touch test && touch test123
+(ls; ps)
+ls; ps; whoami
+lzl || echo "El comando lzl falló"
+echo "Hello World!" > output.txt
+cat output.txt
+echo "Bye World!" > output.txt
+cat output.txt
+echo "Bye World!" >> output.txt
+cat output.txt
+ls -l / &> stdout_and_stderr.txt
+ls -l / &>> stdout_and_stderr.txt
+ls -l / 1> stdout.txt 2> stderr.txt
+cat stdout.txt
+cat stderr.txt 
+lzl 2> error.txt
+cat error.txt
