f8329c6b14084baef89993d0211be8d6c9351317
AreloXD2 <bybuitronva@uide.edu.ec>
2026-06-05 16:50:01 +0000

    wip: Blackhatbash Control Operator &

---
+++ blackhatbash5.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Runs top in the foreground (interactive mode). 
+top
+# Runs top in the background using the ampersand (&).It frees up the terminal so the script can continue immediately.
+top&
+# Kills the command using ID command
+Kill -9 
